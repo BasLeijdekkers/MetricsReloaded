98cf3d6c2b0df601f8ce2dd94e50d5cc8d7aa636
Bas Leijdekkers <basleijdekkers@gmail.com>
2013-10-19 17:55:59 +0200

    fix #21 (Exception when exporting settings in IntelliJ IDEA 12.1.5)

--- src/com/sixrr/metrics/plugin/MetricsPluginApplicationImpl.java
+++ src/com/sixrr/metrics/plugin/MetricsPluginApplicationImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005, Sixth and Red River Software
+ * Copyright 2005-2013 Sixth and Red River Software, Bas Leijdekkers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,10 +18,13 @@ package com.sixrr.metrics.plugin;
 
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.components.ExportableApplicationComponent;
+import com.intellij.util.ArrayUtil;
 import com.sixrr.metrics.utils.MetricsReloadedBundle;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * Allows for exporting metrics profiles.
@@ -35,28 +38,40 @@ import java.io.File;
 public class MetricsPluginApplicationImpl
         implements ExportableApplicationComponent, MetricsPluginApplication {
 
+    @NotNull
+    @Override
     public File[] getExportFiles() {
-        @NonNls final String dirName =
-                PathManager.getConfigPath() + File.separator + "metrics";
+        @NonNls final String dirName = PathManager.getConfigPath() + File.separator + "metrics";
         final File metricsDirectory = new File(dirName);
         final File[] files = metricsDirectory.listFiles();
-        final File[] out = new File[files.length + 1];
+        final File[] out;
+        if (files == null) {
+            out = new File[1];
+        } else {
+            out = new File[files.length + 1];
+            System.arraycopy(files, 0, out, 1, files.length);
+        }
         out[0] = metricsDirectory;
-        System.arraycopy(files, 0, out, 1, files.length);
         return out;
     }
 
+    @NotNull
+    @Override
     public String getPresentableName() {
         return MetricsReloadedBundle.message("metrics.profiles.configuration.presentable.name");
     }
 
+    @NotNull
+    @Override
     public String getComponentName() {
         return "MetricsReloaded";
     }
 
+    @Override
     public void initComponent() {
     }
 
+    @Override
     public void disposeComponent() {
     }
 }
