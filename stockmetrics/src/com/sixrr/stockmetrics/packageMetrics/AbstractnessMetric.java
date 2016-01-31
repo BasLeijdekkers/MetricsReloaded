ae8aae0afd1487d0e3b40ce28ccf5fd25d255aaf
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-01-31 14:44:35 +0100

    report Abstractness and Instability not as a percentage but a number between 0 and 1

--- stockmetrics/src/com/sixrr/stockmetrics/packageMetrics/AbstractnessMetric.java
+++ stockmetrics/src/com/sixrr/stockmetrics/packageMetrics/AbstractnessMetric.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005, Sixth and Red River Software
+ * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ public class AbstractnessMetric extends PackageMetric {
     }
 
     public MetricType getType() {
-        return MetricType.Ratio;
+        return MetricType.Average;
     }
 
     public String getHelpURL() {
