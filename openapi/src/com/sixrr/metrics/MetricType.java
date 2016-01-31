f2915eeafd0b24f4d3225ca55da78759ab5e5075
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-01-31 15:05:00 +0100

    fix javadoc

--- openapi/src/com/sixrr/metrics/MetricType.java
+++ openapi/src/com/sixrr/metrics/MetricType.java
@@ -26,21 +26,25 @@ public enum MetricType {
      * reasonably aggregated to higher levels.  Counts should be positive integer values.
      */
     Count,
+
     /**
      * Use the Score metric type if your metric is some number with no intrinsic meaning other than comparison (i.e. Halstead metrics), or which
-     * cannot be reasonably aggregated (i.e. loop nesting depths).  Scores can be any value.
+     * cannot be reasonably aggregated (i.e. loop nesting depths).  Scores can be any integer value.
      */
     Score,
+
     /**
      * Use the Average metric type if your metric is some computed value representing the average value of some other metric over
      * the reported value.  Averages will be averaged, but not totalled.  Averages can be any value.
      */
     Average,
+
     /**
      * Use the Ratio type if your metric is the ratio of two set sizes, with one set a subset of the other (i.e. comment ratios).
      * Ratio values will be reported as percentages, and the numerators and denominators aggregated separately.
      */
     Ratio,
+
     /**
      * Use the RecursiveRatio type if your metric is the ratio of two set sizes, with one set a subset of the other (i.e. comment ratios),
      * and which is reported on packages recursively.   This is basically the same as the
@@ -48,6 +52,7 @@ public enum MetricType {
      * Ratio values will be reported as percentages, and the numerators and denominators aggregated separately.
      */
     RecursiveRatio,
+
     /**
      * Use the RecursiveCount metric type if your metric is a count of objects in some context (i.e. methods per class) which can be
      * reasonably aggregated to higher levels, and is reported on packages recursively.  This is basically the same as the
