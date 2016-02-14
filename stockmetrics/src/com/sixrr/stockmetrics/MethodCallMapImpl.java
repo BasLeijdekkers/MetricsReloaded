b52b889317cab3e701b568c2306b65d312f9fc56
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-02-14 13:08:55 +0100

    still a few places with faulty product code test

--- stockmetrics/src/com/sixrr/stockmetrics/MethodCallMapImpl.java
+++ stockmetrics/src/com/sixrr/stockmetrics/MethodCallMapImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005, Sixth and Red River Software
+ * Copyright 2005-2016 Sixth and Red River Software
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -87,7 +87,7 @@ public class MethodCallMapImpl implements MethodCallMap {
                         allCalls.add(reference);
                         if (TestUtils.isTest(referencingClass)) {
                             testCalls.add(reference);
-                        } else {
+                        } else if (TestUtils.isProduction(referencingClass)) {
                             productCalls.add(reference);
                         }
                     }
--- stockmetrics/src/com/sixrr/stockmetrics/packageCalculators/NumProductClassesRecursivePackageCalculator.java
+++ stockmetrics/src/com/sixrr/stockmetrics/packageCalculators/NumProductClassesRecursivePackageCalculator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005, Sixth and Red River Software
+ * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -19,10 +19,9 @@ package com.sixrr.stockmetrics.packageCalculators;
 import com.intellij.psi.PsiClass;
 import com.sixrr.metrics.utils.TestUtils;
 
-public class NumProductClassesRecursivePackageCalculator
-        extends ClassCountingRecursivePackageCalculator {
+public class NumProductClassesRecursivePackageCalculator extends ClassCountingRecursivePackageCalculator {
 
     public boolean satisfies(PsiClass aClass) {
-        return !TestUtils.isTest(aClass);
+        return TestUtils.isProduction(aClass);
     }
 }
