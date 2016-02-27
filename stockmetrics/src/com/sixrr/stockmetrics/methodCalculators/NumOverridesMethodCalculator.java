149d90ab497d74a0fe699047a11c1b5cf44d3f6c
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-02-27 13:57:37 +0100

    cleanup and optimization

--- stockmetrics/src/com/sixrr/stockmetrics/methodCalculators/NumOverridesMethodCalculator.java
+++ stockmetrics/src/com/sixrr/stockmetrics/methodCalculators/NumOverridesMethodCalculator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005, Sixth and Red River Software
+ * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,47 +16,49 @@
 
 package com.sixrr.stockmetrics.methodCalculators;
 
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.JavaRecursiveElementVisitor;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.*;
 import com.intellij.psi.search.searches.OverridingMethodsSearch;
 import com.intellij.util.Query;
 import com.sixrr.metrics.utils.MethodUtils;
 
-import java.util.Collection;
-
 public class NumOverridesMethodCalculator extends MethodCalculator {
+
     private int methodNestingDepth = 0;
 
+    @Override
     protected PsiElementVisitor createVisitor() {
         return new Visitor();
     }
 
     private class Visitor extends JavaRecursiveElementVisitor {
 
+        @Override
         public void visitMethod(final PsiMethod method) {
-            final Runnable runnable = new Runnable() {
-                public void run() {
-                    if (methodNestingDepth == 0 && !MethodUtils.isAbstract(method)) {
-                        final Project project = executionContext.getProject();
-                        final GlobalSearchScope globalScope = GlobalSearchScope.projectScope(project);
-                        final Query<PsiMethod> query =
-                                OverridingMethodsSearch.search(method,
-                                        globalScope, true);
-                        final Collection<PsiMethod> overridingMethods =
-                                query.findAll();
-                        postMetric(method, overridingMethods.size());
-                    }
-                    methodNestingDepth++;
+            if (methodNestingDepth == 0 && !MethodUtils.isAbstract(method)) {
+                final int numberOfOverrides = calculateNumberOfOverrides(method);
+                if (numberOfOverrides >= 0) {
+                    postMetric(method, numberOfOverrides);
                 }
-            };
-            final ProgressManager progressManager = ProgressManager.getInstance();
-            progressManager.runProcess(runnable, null);
+            }
+            methodNestingDepth++;
             super.visitMethod(method);
             methodNestingDepth--;
+        }
+
+        private int calculateNumberOfOverrides(PsiMethod method) {
+            if (method.isConstructor() ||
+                    method.hasModifierProperty(PsiModifier.STATIC) ||
+                    method.hasModifierProperty(PsiModifier.FINAL) ||
+                    method.hasModifierProperty(PsiModifier.PRIVATE))
+                return -1;
+            final PsiClass containingClass = method.getContainingClass();
+            if (containingClass == null ||
+                    containingClass instanceof PsiAnonymousClass ||
+                    containingClass.hasModifierProperty(PsiModifier.FINAL)) {
+                return -1;
+            }
+            final Query<PsiMethod> query = OverridingMethodsSearch.search(method);
+            return query.findAll().size();
         }
     }
 }
