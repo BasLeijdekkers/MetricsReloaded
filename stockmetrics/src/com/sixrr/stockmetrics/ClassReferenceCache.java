4f4874f800e6b0f50b7612e1bf0045b084289fc3
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-02-27 20:25:09 +0100

    cleanup

--- stockmetrics/src/com/sixrr/stockmetrics/ClassReferenceCache.java
+++ stockmetrics/src/com/sixrr/stockmetrics/ClassReferenceCache.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.sixrr.stockmetrics;
 
-import com.intellij.psi.*;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.SmartPointerManager;
+import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.openapi.progress.ProgressManager;
 
 import java.util.Collection;
 import java.util.Map;
@@ -12,8 +30,7 @@ import java.util.WeakHashMap;
 public class ClassReferenceCache {
 
     private final Map<SmartPsiElementPointer<PsiClass>, Collection<PsiReference>> cachedReferences =
-            new WeakHashMap<SmartPsiElementPointer<PsiClass>, Collection<PsiReference>>(
-                    256);
+            new WeakHashMap<SmartPsiElementPointer<PsiClass>, Collection<PsiReference>>(256);
 
     public Collection<PsiReference> findClassReferences(final PsiClass aClass) {
         final SmartPointerManager manager = SmartPointerManager.getInstance(aClass.getProject());
@@ -21,16 +38,9 @@ public class ClassReferenceCache {
 
         final Collection<PsiReference> references = cachedReferences.get(pointer);
         if (references == null) {
-            final Runnable runnable = new Runnable() {
-                public void run() {
-                    final SearchScope scope = aClass.getUseScope();
-                    final Collection<PsiReference> references = ReferencesSearch
-                            .search(aClass, scope, true).findAll();
-                    cachedReferences.put(pointer, references);
-                }
-            };
-            final ProgressManager progressManager = ProgressManager.getInstance();
-            progressManager.runProcess(runnable, null);
+            final SearchScope scope = aClass.getUseScope();
+            final Collection<PsiReference> newReferences = ReferencesSearch.search(aClass, scope, false).findAll();
+            cachedReferences.put(pointer, newReferences);
         }
         return cachedReferences.get(pointer);
     }
