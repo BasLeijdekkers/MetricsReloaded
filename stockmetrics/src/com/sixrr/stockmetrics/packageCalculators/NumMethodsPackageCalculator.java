f8753f6336408c23d1a1404cded5726044a749f2
Bas Leijdekkers <basleijdekkers@gmail.com>
2016-03-14 22:56:32 +0100

    add missing package calculator

---
+++ stockmetrics/src/com/sixrr/stockmetrics/packageCalculators/NumMethodsPackageCalculator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.sixrr.stockmetrics.packageCalculators;
+
+import com.intellij.psi.JavaRecursiveElementVisitor;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiJavaFile;
+import com.intellij.psi.PsiMethod;
+
+/**
+ * @author Bas Leijdekkers
+ */
+public class NumMethodsPackageCalculator extends ElementCountPackageCalculator {
+
+    @Override
+    protected PsiElementVisitor createVisitor() {
+        return new Visitor();
+    }
+
+    private class Visitor extends JavaRecursiveElementVisitor {
+
+        @Override
+        public void visitJavaFile(PsiJavaFile file) {
+            super.visitJavaFile(file);
+            createBucket(file);
+        }
+
+        @Override
+        public void visitMethod(PsiMethod method) {
+            super.visitMethod(method);
+            incrementElementCount(method, 1);
+        }
+    }
+}
