5f9e20b1a7e4c13c52e0dda335fab6cb3f581c5f
ArgentumWalker <svidchen-ko@mail.ru>
2017-05-13 15:51:23 +0300

    Add branch count metric

---
+++ stockmetrics/src/com/sixrr/stockmetrics/methodCalculators/BranchCountCalculator.java
@@ -0,0 +1,7 @@
+package com.sixrr.stockmetrics.methodCalculators;
+
+/**
+ * Created by ArgentumWalker on 10.05.2017.
+ */
+public class BranchCountCalculator {
+}
---
+++ stockmetrics/src/com/sixrr/stockmetrics/methodMetrics/BranchCountMetric.java
@@ -0,0 +1,7 @@
+package com.sixrr.stockmetrics.methodMetrics;
+
+/**
+ * Created by ArgentumWalker on 10.05.2017.
+ */
+public class BranchCountMetric {
+}
