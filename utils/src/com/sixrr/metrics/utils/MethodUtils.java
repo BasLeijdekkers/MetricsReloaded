c5cc0a3d6a90edec5ca0d989c1876a43ce84c996
Aleksandr Chudov <chudov.aleksandr94@gmail.com>
2017-05-24 15:49:53 +0300

    Added isGetter() and isSetter() methods

--- utils/src/com/sixrr/metrics/utils/MethodUtils.java
+++ utils/src/com/sixrr/metrics/utils/MethodUtils.java
@@ -17,10 +17,19 @@
 package com.sixrr.metrics.utils;
 
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.PsiFieldImpl;
 import com.intellij.psi.search.searches.SuperMethodsSearch;
 import com.intellij.psi.util.MethodSignatureBackedByPsiMethod;
 import com.intellij.util.Processor;
 import com.intellij.util.Query;
+import org.codehaus.groovy.transform.FieldASTTransformation;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public final class MethodUtils {
 
@@ -79,5 +88,85 @@ public final class MethodUtils {
         }
         out.append(')');
         return out.toString();
+    }
+
+    public static boolean isGetter(final PsiMethod method) {
+        final String methodName = method.getName();
+        final PsiType returnType = method.getReturnType();
+        if (returnType == null || PsiType.VOID.equals(returnType) || method.getParameterList().getParametersCount() > 0 || !methodName.toLowerCase().startsWith("get")) {
+            return false;
+        }
+        final PsiClass aClass = method.getContainingClass();
+        return aClass != null;
+    }
+
+    public static boolean isTrivialGetter(final PsiMethod method) {
+        if (!isGetter(method)) {
+            return false;
+        }
+        final PsiCodeBlock body = method.getBody();
+        if (body == null) {
+            return false;
+        }
+        final PsiStatement[] statements = body.getStatements();
+        return statements.length == 1 && statements[0] instanceof PsiReturnStatement && getUsedFields(statements[0]).size() == 1;
+    }
+
+    public static boolean isSetter(final PsiMethod method) {
+        final String methodName = method.getName();
+        final PsiType returnType = method.getReturnType();
+        final PsiParameterList params = method.getParameterList();
+        if (!PsiType.VOID.equals(returnType) || !methodName.toLowerCase().startsWith("set") || params.getParametersCount() != 1) {
+            return false;
+        }
+        final PsiClass aClass = method.getContainingClass();
+        return aClass != null;
+    }
+
+    public static boolean isTrivialSetter(final PsiMethod method) {
+        if (!isSetter(method)) {
+            return false;
+        }
+        final PsiCodeBlock body = method.getBody();
+        if (body == null) {
+            return false;
+        }
+        final PsiStatement[] statements = body.getStatements();
+        return statements.length == 1 && statements[0] instanceof PsiExpressionStatement && getUsedFields(method).size() == 1;
+    }
+
+    @NotNull
+    public static Set<PsiField> getUsedFields(@NotNull final PsiElement element) {
+        final Set<PsiField> fields = new HashSet<PsiField>();
+        element.accept(new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitReferenceExpression(PsiReferenceExpression expression) {
+                super.visitReferenceExpression(expression);
+                final PsiElement element = expression.resolve();
+                if (element instanceof PsiField) {
+                    fields.add((PsiField) element);
+                }
+            }
+        });
+        return fields;
+    }
+
+    public static boolean isGetterOrSetter(final PsiMethod method) {
+        return isGetter(method) || isSetter(method);
+    }
+
+    public static boolean isTrivialGetterOrSetter(final PsiMethod method) {
+        return isTrivialGetter(method) || isTrivialSetter(method);
+    }
+
+    private static boolean hasField(@NotNull final PsiClass aClass, @NotNull final String name, @NotNull final PsiType type) {
+        final PsiField[] fields = aClass.getAllFields();
+        for (final PsiField field : fields) {
+            final String fieldName = field.getName();
+            if (fieldName != null && fieldName.toLowerCase().equals(name.toLowerCase()) && type.isAssignableFrom(field.getType())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
