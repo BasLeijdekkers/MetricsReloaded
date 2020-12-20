9ca5f3a7573ec269d3deaa6a6a2c01447fe538e7
Bas Leijdekkers <basleijdekkers@gmail.com>
2020-12-20 15:25:21 +0100

    avoid methods that are deprecated for removal

--- utils/src/com/sixrr/metrics/utils/JavaTestUtils.java
+++ utils/src/com/sixrr/metrics/utils/JavaTestUtils.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2005-2016 Sixth and Red River Software, Bas Leijdekkers
+ * Copyright 2005-2020 Sixth and Red River Software, Bas Leijdekkers
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.sixrr.metrics.utils;
 
@@ -54,7 +54,7 @@ public final class JavaTestUtils {
         }
         final PsiMethod[] methods = aClass.getMethods();
         for (PsiMethod method : methods) {
-            if (AnnotationUtil.isAnnotated(method, "org.junit.Test", true)) {
+            if (AnnotationUtil.isAnnotated(method, "org.junit.Test", 0)) {
                 return true;
             }
         }
@@ -62,7 +62,7 @@ public final class JavaTestUtils {
     }
 
     public static boolean isJUnitTestMethod(PsiMethod method) {
-        if (AnnotationUtil.isAnnotated(method, "org.junit.Test", true)) {
+        if (AnnotationUtil.isAnnotated(method, "org.junit.Test", 0)) {
             return true;
         }
         final PsiType returnType = method.getReturnType();
