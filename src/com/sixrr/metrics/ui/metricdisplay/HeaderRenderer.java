683a58e85969181fb5128e09d1e9b9a676c58a72
Bas Leijdekkers <basleijdekkers@gmail.com>
2021-01-29 18:43:22 +0100

    Improve table header rendering

--- src/com/sixrr/metrics/ui/metricdisplay/HeaderRenderer.java
+++ src/com/sixrr/metrics/ui/metricdisplay/HeaderRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2020 Sixth and Red River Software, Bas Leijdekkers
+ * Copyright 2005-2021 Sixth and Red River Software, Bas Leijdekkers
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,13 +17,14 @@
 package com.sixrr.metrics.ui.metricdisplay;
 
 import com.intellij.icons.AllIcons;
-import org.intellij.lang.annotations.MagicConstant;
+import com.intellij.ui.ColoredTableCellRenderer;
+import org.intellij.lang.annotations.JdkConstants;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableCellRenderer;
-import java.awt.*;
 
-class HeaderRenderer extends DefaultTableCellRenderer {
+class HeaderRenderer extends ColoredTableCellRenderer {
 
     private static final Icon UP_ARROW = AllIcons.General.ArrowUp;
     private static final Icon DOWN_ARROW = AllIcons.General.ArrowDown;
@@ -31,45 +32,33 @@ class HeaderRenderer extends DefaultTableCellRenderer {
     private final MetricTableModel model;
     private final int alignment;
 
-    HeaderRenderer(String toolTipText, MetricTableModel model,
-                   @MagicConstant(intValues = {LEFT, RIGHT, CENTER}) int alignment) {
+    HeaderRenderer(String toolTipText, MetricTableModel model, @JdkConstants.HorizontalAlignment int alignment) {
         this.toolTipText = toolTipText;
         this.model = model;
         this.alignment = alignment;
     }
 
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-                                                   int row, int column) {
+    protected void customizeCellRenderer(@NotNull JTable table,
+                                         @Nullable Object value,
+                                         boolean selected,
+                                         boolean hasFocus,
+                                         int row,
+                                         int column) {
         setToolTipText(toolTipText);
-        setHorizontalAlignment(alignment);
-        setHorizontalTextPosition(alignment);
-        setBorder(UIManager.getBorder("TableHeader.cellBorder"));
-        if (!isSelected) {
-            setBackground(table.getTableHeader().getBackground());
-        }
         final int sortColumn = model.getSortColumn();
         final int modelColumn = table.convertColumnIndexToModel(column);
+        setIconOnTheRight(alignment != SwingConstants.RIGHT);
         if (sortColumn == modelColumn) {
+            setTransparentIconBackground(true);
             setIcon(model.isAscending() ? UP_ARROW : DOWN_ARROW);
-        } else {
-            setIcon(null);
         }
-        setFont(table.getTableHeader().getFont());
-        setText(value.toString());
-        return this;
-    }
-
-    @Override
-    public void setBounds(int x, int y, int width, int height) {
-        super.setBounds(x, y, width, height);
-        if (getIcon() != null) {
-            final int textWidth = getFontMetrics(getFont()).stringWidth(getText());
-            final Insets insets = getInsets();
-            final int iconTextGap = width - textWidth - getIcon().getIconWidth() - insets.left - insets.right;
-            setIconTextGap(iconTextGap);
-        } else {
-            setIconTextGap(0);
+        if (!selected) {
+            setBackground(table.getTableHeader().getBackground());
+        }
+        if (value != null) {
+            setTextAlign(alignment);
+            append(value.toString());
         }
     }
 }
